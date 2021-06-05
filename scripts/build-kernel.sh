8e80bd76a5c05ae59b82bbbf66247284dbb61314
Anton Bracke <anton@ju60.de>
2021-06-05 10:35:32 +0200

    chore: improve build kernel script

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -9,16 +9,48 @@ if [ ! -d /lfs/tmp/kernel ]; then
   git clone -b 5.10 --single-branch https://github.com/beagleboard/linux.git /lfs/tmp/kernel
 fi
 
-# compile Kernel
-cd /lfs/tmp/kernel
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- ${kernel_defconfig}
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j$(nproc) zImage dtbs modules
+export CC=arm-linux-gnueabihf-
 
-# install kernel modules and headers
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j$(nproc) modules_install INSTALL_MOD_PATH=/lfs/tmp/rootfs/rootfs
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j$(nproc) headers_install INSTALL_HDR_PATH=/lfs/tmp/rootfs/rootfs/usr
+cd /lfs/tmp/kernel
+make ARCH=arm CROSS_COMPILE=${CC} distclean
+make ARCH=arm CROSS_COMPILE=${CC} clean
+make ARCH=arm CROSS_COMPILE=${CC} bb.org_defconfig
+
+echo "[make ARCH=arm -j$(nproc) CROSS_COMPILE=\"ccache ${CC}\" zImage]"
+make ARCH=arm -j$(nproc) CROSS_COMPILE="ccache ${CC}" zImage
+if [ ! -f arch/arm/boot/zImage ] ; then
+	echo "failed: [arch/arm/boot/zImage]"
+	exit 1
+fi
+
+echo "[make ARCH=arm -j$(nproc) CROSS_COMPILE=\"ccache ${CC}\" modules]"
+make ARCH=arm -j$(nproc) CROSS_COMPILE="ccache ${CC}" modules
+if [ ! -f drivers/spi/spidev.ko ] ; then
+	echo "failed: [drivers/spi/spidev.ko]"
+	exit 1
+fi
+
+echo "[make ARCH=arm CROSS_COMPILE=\"ccache ${CC}\" dtbs]"
+make ARCH=arm CROSS_COMPILE="ccache ${CC}" dtbs
+if [ ! -f arch/arm/boot/dts/am335x-boneblack.dtb ] ; then
+	echo "failed: [arch/arm/boot/dts/am335x-boneblack.dtb]"
+	exit 1
+else
+	if [ -f arch/arm/boot/dts/am335x-pocketbeagle.dts ] ; then
+		if [ ! -f arch/arm/boot/dts/am335x-pocketbeagle.dtb ] ; then
+			echo "failed: [arch/arm/boot/dts/am335x-pocketbeagle.dtb]"
+			exit 1
+		fi
+	fi
+fi
+
+make ARCH=arm CROSS_COMPILE=${CC} clean
+# rm -rf ./gcc-* || true
+
+# # install kernel modules and headers
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j$(nproc) modules_install INSTALL_MOD_PATH=/lfs/tmp/rootfs/rootfs
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j$(nproc) headers_install INSTALL_HDR_PATH=/lfs/tmp/rootfs/rootfs/usr
 
 # install kernel binary and device tree
-cp /lfs/tmp/kernel/arch/arm/boot/zImage /lfs/tmp/rootfs/rootfs/boot
-cp /lfs/tmp/kernel/arch/arm/boot/dts/am335x-boneblack.dtb /lfs/tmp/rootfs/rootfs/boot
+cp arch/arm/boot/zImage /lfs/tmp/rootfs/rootfs/boot
+cp arch/arm/boot/dts/am335x-boneblack.dtb /lfs/tmp/rootfs/rootfs/boot
