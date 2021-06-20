10484321e0acbfcc02bb9921a8f41fb6174697b6
Anton Bracke <anton@ju60.de>
2021-06-20 13:05:12 +0200

    feat: add ctag driver

--- scripts/prepare-kernel.sh
+++ scripts/prepare-kernel.sh
@@ -12,3 +12,18 @@ if [ ! -d /lfs/tmp/kernel ]; then
 else
   echo -e "${ECHO_PREFIX}Kernel files already exist"
 fi
+
+cd /lfs/tmp/kernel
+
+if [ "$KERNEL_BRANCH" == "5.10" ] || [ "$KERNEL_BRANCH" == "5.10-rt" ]; then
+  echo -e "${ECHO_PREFIX}Patching kernel with CTAG-2.4 driver ..."
+  patch -t -s -p0 < /lfs/resources/ctag-2.4/ctag-2.4-k5.10.patch || true
+fi
+
+make ARCH=arm CROSS_COMPILE=${CC} clean
+make ARCH=arm CROSS_COMPILE=${CC} bb.org_defconfig
+
+if [ "$KERNEL_BRANCH" == "5.10" ] || [ "$KERNEL_BRANCH" == "5.10-rt" ]; then
+  echo -e "${ECHO_PREFIX}Enabling CTAG-2.4 driver ..."
+  patch -t /lfs/tmp/kernel /lfs/resources/ctag-2.4/.config.patch || true
+fi
